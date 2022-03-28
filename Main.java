a03889c96ca9bccf53b9447578bf876b299e8a5b
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-03-28 18:35:36 +0000

    opis

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
     String znak="*";// robimy sobie zmienna typu string której przypisujemy nasz znak
     for(int x=0;x<7;x++){// petla for zmienna x warunek trwania x<7 i inkrementacja czyli dodawanie 1
       for(int y=0;y<7*2;y++){ // tutaj to samo co wyzej tylko inny warunek i zawarty jeszcze w tej petli powyzej
-        if(y<(7-x)||y>(7+x)){// jesli warunek jest spełiniony to drukuje na ekran pusty znak? a jeśeli nie to drukuje *
+        if(y<(7-x)||y>(7+x)){// jesli warunek jest spełiniony to drukuje na ekran pusty znak? a jeśeli nie to drukuje *, a i || to coś to suma logiczna czyli tak prościej dodawanie
           System.out.print("");
         }
         else{
